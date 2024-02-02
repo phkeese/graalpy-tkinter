48199a4b21082e44d4b5dbbd369eb95751775c44
Philipp Keese <philipp.keese@student.hpi.de>
2024-02-02 18:33:05 +0100

    fix typo

--- _tkinter/app.py
+++ _tkinter/app.py
@@ -110,7 +110,7 @@ class TkApp(object):
         self.interp = tklib.Tcl_CreateInterp()
         self._wantobjects = wantobjects
         # "threaded" is an optionally present member of "tcl_platform" when TCL was compiled with threading.
-        # Tcl_GetVar2Ex should NULL when "threaded" is not present, so casting to a bool here is doing an implicit NULL-check.
+        # Tcl_GetVar2Ex should return NULL when "threaded" is not present, so casting to a bool here is doing an implicit NULL-check.
         self.threaded = bool(tklib.Tcl_GetVar2Ex(
             self.interp, b"tcl_platform", b"threaded",
             tklib.TCL_GLOBAL_ONLY))
